9301589fe2650764faae8350224fa21e8cccee62
rjmorales07 <reinaldo1990junior@gmail.com>
2022-09-08 12:27:59 -0500

    testlaravel completo

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+//use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_login_form(){
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+}
